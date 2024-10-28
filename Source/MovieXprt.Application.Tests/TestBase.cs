9f552d017087033697da25afbe4e3ab0bd53111e
Wouter <wvanhees35@hotmail.com>
2024-10-28 11:17:37 +0100

    fix application testbase because of DateOnly limitations

--- Source/MovieXprt.Application.Tests/TestBase.cs
+++ Source/MovieXprt.Application.Tests/TestBase.cs
@@ -1,10 +1,19 @@
 ﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
 
 namespace MovieXprt.Application.Tests
 {
     public class TestBase<T> where T : class
     {
-        protected Fixture Fixture { get; init; } = new Fixture();
+        protected Fixture Fixture { get; }
+
+        public TestBase()
+        {
+            Fixture = new Fixture();
+            Fixture.Customize(new AutoNSubstituteCustomization());
+            // https://github.com/AutoFixture/AutoFixture/pull/1305
+            Fixture.Customize<DateOnly>(composer => composer.FromFactory<DateTime>(DateOnly.FromDateTime));
+        }
 
         protected virtual T CreateSut()
         {
